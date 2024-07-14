b5a5fd4a9a7dccf17afe8ff522ed9d462f649faa
ericaheeja <heejaerica@gmail.com>
2024-07-14 16:21:27 -0700

    Edit Chat.jsx and add additional components

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import "./Join.css";
 
-function Join() {
+export default function SignIn() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
 
@@ -28,10 +28,10 @@ function Join() {
           />
         </div>
         <Link
-          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
           to={`/chat?name=${name}&room=${room}`}
         >
-          <button className="button mt-20" type="submit">
+          <button className={"button mt-20"} type="submit">
             Sign In
           </button>
         </Link>
@@ -39,5 +39,3 @@ function Join() {
     </div>
   );
 }
-
-export default Join;
